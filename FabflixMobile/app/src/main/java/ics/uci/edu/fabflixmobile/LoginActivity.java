5807052d4ee8c1233c234218857610fe7f01a994
tyao117 <tyao117@gmail.com>
2018-06-05 23:18:03 -0700

    missing the toast message

--- FabflixMobile/app/src/main/java/ics/uci/edu/fabflixmobile/LoginActivity.java
+++ FabflixMobile/app/src/main/java/ics/uci/edu/fabflixmobile/LoginActivity.java
@@ -75,8 +75,13 @@ public class LoginActivity extends AppCompatActivity {
 
                                 Toast.makeText(LoginActivity.this, "Login Successful", Toast.LENGTH_SHORT).show();
                             }
+                            else
+                            {
+                                Toast.makeText(LoginActivity.this, "Login failed", Toast.LENGTH_SHORT).show();
+                            }
                         } catch (JSONException e) {
                             e.printStackTrace();
+                            Toast.makeText(LoginActivity.this, "Unknown Error", Toast.LENGTH_SHORT).show();
                         }
                     }
                 },
