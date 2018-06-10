04a947a5839fadcf562e6f325b3040bdaa9b3497
tyao117 <tyao117@gmail.com>
2018-06-10 13:15:53 -0700

    started collecting results

--- getAllAvg.sh
+++ getAllAvg.sh
@@ -2,7 +2,9 @@
 mkdir temp1;
 python3 findAvg.py < JDBCMLNoPool.txt > temp1/JDBCNoPoolResult.txt
 python3 findAvg.py < JDBCMLNOPS.txt > temp1/JDBCNoPSResult.txt
-python3 findAvg.py < JDBCML.txt > temp1/JDBCMLResult.txt
-python3 findAvg.py < SearchServletML.txt > temp1/SearchMLResult.txt
+python3 findAvg.py < JDBCML1.txt > temp1/JDBCMLResult1.txt
+python3 findAvg.py < JDBCML10.txt > temp1/JDBCMLResult10.txt
+python3 findAvg.py < SearchServletML1.txt > temp1/SearchMLResult1.txt
+python3 findAvg.py < SearchServletML10.txt > temp1/SearchMLResult10.txt
 python3 findAvg.py < SearchServletMLNOPS.txt > temp1/SearchMLNoPSResult.txt
 python3 findAvg.py < SearchServletMLNoPool.txt > temp1/SearchMLNoPoolResult.txt
